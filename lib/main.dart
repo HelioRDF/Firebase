84f4bd34aa625fae0fc537c3a8b9f36dbd2c5626
Helio Franca <heliordf@hotmail.com>
2020-11-22 15:51:35 -0300

    Ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  salvar();
-  runApp(App());
+  //salvarOuAtualizarFireStore();
+  addFireStore();
+  runApp(MaterialApp(
+    home: App(),
+  ));
 }
 
-void salvar() async {
+Future<void>  salvarOuAtualizarFireStore() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await Firestore.instance
       .collection("Usuarios")
       .document("pontuacao")
-      .setData({"carlos": "80", "silvana": "340"});
+      .setData({"helio": "888", "aline": "888"});
+}
+
+Future<void> addFireStore() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseFirestore db = await FirebaseFirestore.instance;
+  DocumentReference ref = await db.collection("noticias")
+  .add({
+    "titulo" : "Ondas de calor em São Paulo",
+    "descricao" : "texto de exemplo..."
+  });
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FireBase"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Container(
+        child: Column(children: [
+          Text("Hello Word!")
+        ],),
+      ),
+    );
   }
 }
